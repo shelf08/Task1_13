7b4b6552105a4aef8f144f9054dc78f38931a55f
shelf08 <vip.lobtsov@gmail.com>
2023-11-06 00:08:20 +0300

    belive working task, that help me to take good mark

--- src/ru/vsu/cs/util/lobtsov_d_a/task1_14/CycleLinkedList.java
+++ src/ru/vsu/cs/util/lobtsov_d_a/task1_14/CycleLinkedList.java
@@ -1,9 +1,38 @@
 package ru.vsu.cs.util.lobtsov_d_a.task1_14;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CycleLinkedList<T> extends DoubleLinkedList<T>{
-    @Override
-    protected Node<T> getNode(int i) {
-        int j = (Math.abs(i) % size + size) % size;
-        return super.getNode(j);
+
+    public static class Node<T> {
+        public T val;
+        public Node<T> next;
+
+        public Node(T val) {
+            this(val, null);
+        }
+
+        public Node(T val, Node<T> next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static Node<Integer> nextNode(Node<Integer> node) {
+        if (node.next == null) {
+            return node;
+        }
+        return node.next;
+    }
+
+    public static boolean hasCycle(Node<Integer> nodes) {
+        Node<Integer> next = nextNode(nodes);
+        Node<Integer> nextNext = nextNode(nextNode(nodes));
+        while (next != nextNext && nextNext.next != null) {
+            next = nextNode(next);
+            nextNext = nextNode(nextNode(nextNext));
+        }
+        return nextNext.next != null;
     }
 }
